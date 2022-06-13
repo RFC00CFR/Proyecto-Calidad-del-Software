490788e416f27401f3e5023a06c4f9bf13c4a1d7
Roberto <Roberto@DESKTOP-PCB1OR7>
2022-06-12 20:42:46 -0600

    02 Anteproyecto Calidad del software

--- Test-SeleniumJava/src/newpackage/MyClass.java
+++ Test-SeleniumJava/src/newpackage/MyClass.java
@@ -1,6 +1,26 @@
 package newpackage;
 
-public class MyClass {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-	
+public class MyClass {
+public static void main(String[] args) {
+
+ System.setProperty("webdriver.chrome.driver", "C:\\Users\\Roberto\\Downloads\\Nueva carpeta\\chromedriver.exe");
+WebDriver driver = new ChromeDriver();
+String baseUrl = "https://demo.prestashop.com/";
+String expectedTitle = "PrestaShop Live Demo";
+String actualTitle = "";
+
+driver.get(baseUrl);
+
+actualTitle = driver.getTitle();
+
+ if (actualTitle.contentEquals(expectedTitle)){
+System.out.println("Test Passed!");
+} else {
+System.out.println("Test Failed");
+}
+//driver.close();
+}
 }
